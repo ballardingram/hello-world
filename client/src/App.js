db76f189c3f085d58edf5a2941f768c5514febcc
William Stock <grim118@hotmail.com>
2022-07-05 19:17:20 -0500

    added dependencies to client and updated code in app.js for react router and added AuthService (jwt) to utils/auth.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,47 @@
 import './App.css';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 import Terms from "./pages/TermsAndConditions";
 
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>
-        <Terms></Terms>
-      </div>
+      <Router>
+        <div>
+          {/* place for header if needed */}
+          <div>
+            <Routes>
+              <Route
+                path="/terms"
+                element={<Terms />}
+              />
+            </Routes>
+          </div>
+          {/* place for footer if needed */}
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
