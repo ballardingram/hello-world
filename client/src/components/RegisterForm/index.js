3ee59911a1e620b3275588550647c3b35baf04fa
CassDoes <c.doexo@gmail.com>
2022-07-09 00:42:32 -0500

    updated register component style and added facebook and github buttons for sign in

--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Switch from '../Toggle';
+import { Icon } from '@iconify/react';
+import githubFill from '@iconify/icons-akar-icons/github-fill'
+import facebookIcon from '@iconify/icons-bi/facebook';
 
 
 const RegisterForm = () => {
@@ -43,19 +46,32 @@ const RegisterForm = () => {
                   id='password'
                 />
               </div>
-                <div className='mt-8 flex content-center justify-between' id='terms-label'>
-                  <div className='inline-block ml-4 text-sm font-bold h-full content-center'>
-                    Agree to terms & conditions
-                  </div>
-                    <Switch></Switch>
-                </div>               
-                <div className='mt-2'>
-                    <button 
-                      className='w-full px-4 pb-1 tracking-wide text-white rounded-lg text-2xl font-thin'
-                      id='form-btn'>
-                          Create Account
-                    </button>
+
+              <div className='mt-2'>
+                  <button 
+                    className='w-full py-1 mt-2 tracking-wide text-white rounded-lg text-2xl font-thin'
+                    id='form-btn'>
+                        Create Account
+                  </button>
+              </div>
+
+              <p className='text-center mt-5 font-semibold underline'>or</p>
+              
+              <div className='mt-2'>
+                <p className="text-sm mr-2 font-semibold text-center mb-2 tracking-wider">Sign up with:</p>
+                <div className='flex justify-center'>
+                  <a href='/' className='facebook mx-3'><Icon icon={facebookIcon} height="30" /></a>
+                  <a href='/' className='github mx-3'><Icon icon={githubFill} height="30" /></a>
                 </div>
+
+              </div>
+
+              <div className='mt-10 flex content-center justify-between' id='terms-label'>
+                <div className='inline-block ml-4 text-sm h-full content-center'>
+                  Agree to terms & conditions
+                </div>
+                    <Switch></Switch>
+              </div> 
             </form>
 
         </div>
