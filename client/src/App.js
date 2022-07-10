323076c29b582bfe2b703bc9b15702d9d421047d
William Stock <grim118@hotmail.com>
2022-07-10 14:08:40 -0500

    changed landing page back to the 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -48,7 +48,7 @@ function App() {
                 element={<Register />}
               />
               <Route
-                path="/"
+                path="/login"
                 element={<Login />}
               />
               <Route
