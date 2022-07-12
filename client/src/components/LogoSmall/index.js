6dafcb01dd7173f434208529de7f1af1cd4be676
CassDoes <96797348+CassDoes@users.noreply.github.com>
2022-07-12 02:17:54 -0500

    Delete index.js

--- client/src/components/LogoSmall/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const LogoSmall = () => {
-  return (
-    <img src={require('../../assets/mobile/logo256.png')} alt="hello world logo"/>
-  );
-}
-
-export default LogoSmall;
