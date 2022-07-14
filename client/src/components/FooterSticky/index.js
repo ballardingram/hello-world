a46e1bb4915bd18074ddc4b355a77c63c7351198
CassDoes <c.doexo@gmail.com>
2022-07-14 07:36:18 -0500

    updated footer content to display differently on larger screens

--- client/src/components/FooterSticky/index.js
+++ client/src/components/FooterSticky/index.js
@@ -9,15 +9,24 @@ import searchLoading from '@iconify/icons-gg/search-loading';
 const FooterSticky = () => {
   return (
     
-    <footer className="h-16 bg-gray-300 rounded-t-xl display-contents">
+  <div>
+    <div className='p-2 rounded-t-2xl'>
+    <div className='hidden lg:contents text-xl font-semibold'>
+      <a href='/terms' className='mx-5 color-sY'>Terms</a>
+      <a href='about' className='mx-5 color-sY'>About</a>
+    </div>
+    </div>
+ 
+    <footer className="h-16 bg-gray-300 rounded-t-xl md:hidden">
       <div className='flex flex-row justify-around items-center py-2'>
-        <a href='/profile'><Icon icon={homeIcon} height="42" /></a>
-        <a href='/'><Icon icon={searchLoading} height="42" className='invisible sm:visible'/></a>
-        <a href='/projecthub'><Icon icon={codeIcon} height="32" /></a>
-        <a href='/'><Icon icon={chatText} height="36" className='invisible sm:visible'/></a>
-        <a href='#open'><Icon icon={hamburgerIcon} height="42" /></a>
+        <a href='/profile'><Icon icon={homeIcon} height="42" className='display-contents md:hidden'/></a>
+        <a href='/'><Icon icon={searchLoading} height="42" className='invisible sm:visible md:hidden'/></a>
+        <a href='/projecthub'><Icon icon={codeIcon} height="32" className='display-contents md:hidden'/></a>
+        <a href='/'><Icon icon={chatText} height="36" className='invisible sm:visible md:hidden'/></a>
+        <a href='/'><Icon icon={hamburgerIcon} height="42" className='display-contents md:hidden'/></a>
       </div>
     </footer>
+  </div>
   );
 }
 export default FooterSticky;
