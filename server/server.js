e63926934e900518a47c67f0b1f65d12a4412dd8
Ramu Makkena <MRamuMailer@gmail.com>
2022-07-14 23:42:28 -0500

    Adding prod variable check

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const passport = require("passport");
 const { ApolloServer } = require("apollo-server-express");
+const path = require('path');
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
@@ -21,6 +22,10 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 app.use(passport.initialize());
 app.use(routes);
 // Create a new instance of an Apollo server with the GraphQL schema
